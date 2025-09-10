3457e8a33c1632ac7a4d9e1c0c5336f14260fc3e
HritikNagpal07 <87565187+HritikNagpal07@users.noreply.github.com>
2025-09-11 00:47:32 +0530

    postcss.config.js

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+// This is a configuration file for PostCSS, which is a tool that processes our CSS.
+// It tells it to use the Tailwind CSS plugin and the Autoprefixer plugin.
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
