8e8d3ca3dd59e971c625d416193825f850aa408e
HritikNagpal07 <87565187+HritikNagpal07@users.noreply.github.com>
2025-09-11 00:46:34 +0530

    tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // This tells Tailwind to look for classes in all our React files
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
